84b1e60e1a0968b7a5491cec7b48cf9346a99541
Johana Pokorná <johanapokorna@Johana-Air.home>
2023-06-03 12:38:45 +0200

    přidání funkce pro klikání, kompletní zprovoznění kódu

--- src/App/app.jsx
+++ src/App/app.jsx
@@ -1,45 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Joke } from '../Joke/joke';
+import { jokes } from '../jokes';
 
+// 14.úloha
 export const App = () => {
-  const [upLikes, setUpLikes] = useState(0);
-  const [downLikes, setDownLikes] = useState(0);
   return (
     <div className="container">
-      <div className="joke">
-        <div className="joke__body">
-          <div className="joke__user">
-            <img
-              className="user-avatar"
-              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-            />
-            <p className="user-name">Neroxx</p>
-          </div>
-
-          <p className="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
-        </div>
-        <div className="joke__likes">
-          <button
-            id="btn-up"
-            className="btn-like btn-like--up"
-            onClick={() => setUpLikes(upLikes + 1)}
-          ></button>
-          <span id="likes-up" className="likes-count likes-count--up">
-            {upLikes}
-          </span>
-          <button
-            id="btn-down"
-            className="btn-like btn-like--down"
-            onClick={() => setDownLikes(downLikes + 1)}
-          ></button>
-          <span id="likes-down" className="likes-count likes-count--down">
-            {downLikes}
-          </span>
-        </div>
-      </div>
+      {jokes.map((oneJoke) => (
+        <Joke
+          key={oneJoke.id}
+          userAvatar={oneJoke.avatar}
+          userName={oneJoke.name}
+          text={oneJoke.text}
+          likes={oneJoke.likes}
+          dislikes={oneJoke.dislikes}
+        />
+      ))}
     </div>
   );
 };
