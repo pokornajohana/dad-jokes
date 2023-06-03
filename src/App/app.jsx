a238f42445806f10ee337a350cbbbfbbb10d12d0
Johana Pokorná <johanapokorna@Johana-Air.home>
2023-06-03 11:52:14 +0200

    funkční kód - zprovoznění tlačítek

--- src/App/app.jsx
+++ src/App/app.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 export const App = () => {
+  const [upLikes, setUpLikes] = useState(0);
+  const [downLikes, setDownLikes] = useState(0);
   return (
     <div className="container">
       <div className="joke">
@@ -20,13 +22,21 @@ export const App = () => {
           </p>
         </div>
         <div className="joke__likes">
-          <button id="btn-up" className="btn-like btn-like--up"></button>
+          <button
+            id="btn-up"
+            className="btn-like btn-like--up"
+            onClick={() => setUpLikes(upLikes + 1)}
+          ></button>
           <span id="likes-up" className="likes-count likes-count--up">
-            0
+            {upLikes}
           </span>
-          <button id="btn-down" className="btn-like btn-like--down"></button>
+          <button
+            id="btn-down"
+            className="btn-like btn-like--down"
+            onClick={() => setDownLikes(downLikes + 1)}
+          ></button>
           <span id="likes-down" className="likes-count likes-count--down">
-            0
+            {downLikes}
           </span>
         </div>
       </div>
